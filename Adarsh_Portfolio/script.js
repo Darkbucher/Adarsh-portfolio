8684ea0c891305245bcb73c23165138462e7b302
ADARSH SINGH <109370113+Darkbucher@users.noreply.github.com>
2024-11-26 23:42:12 +0530

    all in

---
+++ Adarsh_Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer . . ." , "Programmer . . ." , "Web Developer . . ." , "Learner . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
